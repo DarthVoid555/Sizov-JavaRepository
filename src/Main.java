f6a39b59075f7bee7e53f9f99a92fe737fa4053a
MAX <makssizov200206@gmail.com>
2025-10-27 20:06:24 +0700

    Task 7 completed

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,11 @@
 public class Main {
     public static void main(String[] args) {
-        String name = System.console().readLine("What is your name?\n> ");
-        System.out.println("Hello, " + name + "!");
+        FileAnalyzer analyzer = new FileAnalyzer();
+
+        System.out.println("Анализ файла test.txt:");
+        analyzer.analyze("test.txt");
+
+        System.out.println("\nАнализ несуществующего файла:");
+        analyzer.analyze("no_file.txt");
     }
 }
