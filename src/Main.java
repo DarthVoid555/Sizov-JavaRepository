dc794712a1f19894ed417ab409693e2adaa971fb
MAX <makssizov200206@gmail.com>
2025-11-30 21:45:30 +0700

    completed

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,11 @@
 public class Main {
     public static void main(String[] args) {
-        String name = System.console().readLine("What is your name?\n> ");
-        System.out.println("Hello, " + name + "!");
+        StudentGrades grades = new StudentGrades();
+
+        System.out.println("Анализ оценок студентов:");
+        grades.analyze("grades.txt");
+
+        System.out.println("\nЛучший студент: " + grades.bestStudent());
+        System.out.println("Худший студент: " + grades.worstStudent());
     }
 }
